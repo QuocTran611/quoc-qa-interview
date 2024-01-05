4e1a7ca84e879e90aa0a9d938f6c003086a15470
QuocTran611 <tran.ngocquoc@amaris.com>
2024-01-05 17:22:51 +0700

    Add detail comments

--- __tests__/testAuth.js
+++ __tests__/testAuth.js
@@ -16,19 +16,26 @@ describe('Testing Authorization', () => {
   });
 
   it('Verify login sucessfully with valid Reward Moderator credentials', async () => {
+
+    //Access to the dashboard
     await driver.get('https://www.perxtech.io/dashboard');
 
+    //Find credential elements including username and password
     const usernameInput = await driver.findElement(By.id('email'));
-    const passwordInput = await driver.findElement(By.id('password'));
+    const passwordInput = await driver.findElement(By.id('password')); 
+    //Find Login button element
     const loginButton = await driver.findElement(By.className('ant-btn'));
 
+    //Input the email and password for login
     await usernameInput.sendKeys('thao+reward-test-qa-interview@perxtech.com');
     await passwordInput.sendKeys('reward_admin');
+
+    //Clik to the login button
     await loginButton.click();
 
     // Wait for the page to load after login (customize as needed)
     await driver.wait(until.urlIs('https://dashboard.perxtech.io/dashboard/p/rewards/list'), 5000);
-
+    
     // Assertions for successful login
     expect(await driver.getCurrentUrl()).toBe('https://dashboard.perxtech.io/dashboard/p/rewards/list');
     // Add more assertions if needed
